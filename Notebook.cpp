1e6ca235ae03860395169fe03907fc3499a737d8
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2022-03-22 01:03:43 +0200

    Implement with HASH-MAP

--- Notebook.cpp
+++ Notebook.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <unordered_map>
 #include "Notebook.hpp"
 #include "Direction.hpp"
 using ariel::Direction;
@@ -9,52 +10,12 @@ using namespace std;
 const int MAX=100;
 const int FIRST=0;
 const char ASSIGN='_';
- Node::Node(int num)
-    {
-        this->page=num;
-        this->note.assign(MAX,vector<char>(MAX,ASSIGN));
-        this->next=NULL;
-    }
+
 namespace ariel{
 
     Notebook::Notebook()
     {
-        this->notebook=new Node(FIRST);
-    }
-    Notebook::~Notebook()
-    {
-        Node* temp=notebook;
-            while(temp!=NULL){
-                notebook=notebook->next;
-                delete temp;
-                temp=notebook;
-            }
-    }
-    void Notebook::insert(int number){
-        Node* temp=new Node(number);
-        if(notebook==NULL)
-        {
-            notebook=temp;
-            return;
-        }
-        Node* tempN=notebook;
-        while(tempN->next!=NULL)
-        {
-            tempN=tempN->next;
-        }
-        tempN->next=temp;
-
-    }
-    Node* Notebook::get(int number){
-        Node* tempN=notebook;
-        while(tempN!=NULL)
-        {
-            if(tempN->page==number){
-                break;
-            }
-            tempN=tempN->next;
-        }
-        return tempN;
+        this->notebook[0].assign(MAX,vector<char>(MAX,ASSIGN));
     }
     void Notebook::write(int page,int row,int column, Direction dir,const string &text){
       
--- Notebook.hpp
+++ Notebook.hpp
@@ -2,31 +2,21 @@
 #include <iostream>
 #include "Direction.hpp"
 #include <vector>
+#include <unordered_map>
 #include <cstdlib>
 using ariel::Direction;
 using namespace std;
 
-  class Node{
-        public:
-            int page;
-            vector<vector<char>> note;
-            Node* next;
-            Node(int num);
-            
-    };
 namespace ariel{
    
     class Notebook{
-        Node* notebook;
+        unordered_map<int,vector<vector<char>>> notebook;
         public:
             Notebook();
-            ~Notebook();
             static void write(int page,int row,int column, Direction dir,const string &text);
             static string read(int page,int row,int column, Direction dir,int length);
             static void erase(int page,int row,int column, Direction dir,int length);
             static string show(int page);
-            void insert(int number);
-             Node* get(int number);
     };
 
 };
